67b31d95d95eae653ad09f803159b275cab9d843
Asthaa2002 <kirteeeeesri2002@gmail.com>
2024-03-14 20:35:26 +0530

    fbn

--- app.js
+++ app.js
@@ -17,12 +17,17 @@ app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 app.use("/post",postRoutes);
 
+app.use("/", (req, res) => {
+  console.log("Working ");
+  res.send("WORKING");
+});
+
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(Server is running on port ${PORT});
     });
   })
   .catch((err) => console.log(err));
